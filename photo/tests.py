759102652d6032537e043dd5613e57894eb82cf3
usanase Francoise <fusanasencoise@gmail.com>
2019-10-13 18:26:05 +0200

    def test update location

---
+++ photo/tests.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from .models import Location,Image,Category
+import datetime as dt
+
+class LocationTestCase(TestCase):
+    def setUp(self):
+        self.Kigali =Location(photo_location='Rwanda')
+        self.Kigali.save_location()
+
+    def test_updating_location(self):
+        location=Location.get_location_id(self.Kigali.id)
+        location.update_location('Canada')
+        location=Location.get_location_id(self.Kigali.id)
+        self.assertTrue(location.photo_location == 'Canada')
+        
